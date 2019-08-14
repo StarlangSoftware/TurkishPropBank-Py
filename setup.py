b94bbd38ba0e9eed43914b6ceaf24086c69134db
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-08-14 20:44:49 +0300

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-PropBank',
-    version='1.0.0',
+    version='1.0.1',
     packages=['PropBank'],
     url='https://github.com/olcaytaner/TurkishPropbank-Py',
     license='',
