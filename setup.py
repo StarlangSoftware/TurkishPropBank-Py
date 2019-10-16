c579ef042098afb1e6615de118eeb4f95d16d74a
olcaytaner <olcaytaner@isikun.edu.tr>
2019-10-16 15:42:17 +0300

    Removed edilgen and ettirgen verbs.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-PropBank',
-    version='1.0.2',
+    version='1.0.3',
     packages=['PropBank'],
     url='https://github.com/olcaytaner/TurkishPropbank-Py',
     license='',
