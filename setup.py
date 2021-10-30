a38b428592db5bc678e2abf28e300d7b4c515c95
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:28:46 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-PropBank',
-    version='1.0.16',
+    version='1.0.17',
     packages=['PropBank'],
     url='https://github.com/StarlangSoftware/TurkishPropbank-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='Turkish PropBank'
+    description='Turkish PropBank',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
